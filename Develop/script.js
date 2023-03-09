324778273e5bc9eb47daeb11ac4da7df6966c5af
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-08 18:26:03 -0700

    added ecmascript 6 code

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,16 @@
 // Assignment code here
+function generatePassword() {
+
+alert(values);
+
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
 // ------------------
 
