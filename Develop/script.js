2c23d149b7f5264b58aaccd13f9dffd4d84c2a3a
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-08 17:24:42 -0700

    follow juni learning tutorial to see if it works

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,47 @@
 // Assignment code here
 
+import java.util.Scanner;
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+class Main {
+  public static vid main (String[] args) [
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    Scanner in = new Scanner(System.in);
 
-  passwordText.value = password;
+    // ask user password criteria
+    System.out.print("How many random passwords do you want generated?");
+    int total = in.nextInt();
 
+
+  ]
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// ask the user to enter total numer of random passwords they want generated
+
+// ask he user how many charas long tey want the passwords
+
+// create a function that generates a random chara, which can be a
+// number, lower case, or uppercase (and syumbols?)
+
+// make the random password by looping ???
+
+// store each random password you generate in an array
+
+// create a function to print out your array of passwords
+
+
+// -----------------------------------------
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Password Generator Starter Code
+
+
+## credits
+https://www.tutorialspoint.com/Generating-password-in-Java
+
+juni learning: https://www.youtube.com/watch?v=3Vg2J1CWs2Q&ab_channel=JuniLearning
