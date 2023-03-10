0a2c01b99333d4174146333bbecde97e3c2c4045
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-09 21:31:49 -0700

    added some for, while, and do/while loops

--- Develop/script.js
+++ Develop/script.js
@@ -8,21 +8,67 @@ function generatePassword() {
   let characterSpecial = window.confirm ("Would you like special characters?");
   let characterLength = window.prompt ("How many characters long would you like your password?");
 
-var length = characterLength;
-lower = "aghijklwbcmnopqrstuvdefxyz";
-upper = "ASTHIJFGNOZBCDPQUVWRKLMXEY";
-numbers = "1567089234";
-special = "!@#$%^&*";
-retVal = "";
+  var length = characterLength;
+  lower = "aghijklwbcmnopqrstuvdefxyz";
+  upper = "ASTHIJFGNOZBCDPQUVWRKLMXEY";
+  numbers = "1567089234";
+  special = "!@#$%&*";
+  retVal = "";
+
+  // ------------------------------------------------------------------------
+
+  // for (var i = 0, n = characterLength; i < length; ++i) {
+  //     retVal += lower.charAt(Math.random() * n)
+  //     retVal += upper.charAt(Math.random() * n)
+  //     retVal += numbers.charAt(Math.random() * n)
+  //     retVal += special.charAt(Math.random() * n)
+  // };
+
+  // ------------------------------------------------------------------------
 
   for (var i = 0, n = characterLength; i < length; ++i) {
-      retVal += lower.charAt(Math.random() * n)
-      retVal += upper.charAt(Math.random() * n)
-      retVal += numbers.charAt(Math.random() * n)
-      retVal += special.charAt(Math.random() * n)
+  do {
+  retVal += lower.charAt(Math.random() * n)
+  }
+  while (characterLower = true);
+
+  do {
+  retVal += upper.charAt(Math.random() * n)
+  }
+  while (characterUpper = true);
+
+  do {
+  retVal += numbers.charAt(Math.random() * n)
+  }
+  while (characterNumbers = true);
+
+  do {
+  retVal += special.charAt(Math.random() * n)
+  }
+  while (characterSpecial = true);
   };
+
+
+// ------------------------------------------------------------------------
+
+  // while(characterLower = true) {
+  //   retVal += lower.charAt(Math.random() * n)
+  // }
+
+  // ------------------------------------------------------------------------
+
+  // recieve user input prompt results
+  // while lower result = true, include the lower numbers
+  // while upper result = true, include
+  // while numbers result ttue, include
+  // while special, include
+
+  // ------------------------------------------------------------------------
+
   return retVal.substring(0, length);
 };
+
+// ------------------------------------------------------------------------
 
 // IDEA: SAVE ALL OF THE THINGS AND THEN PUT 'IF TRUE LOWER CASE' ????
 // AND EVERY IF WOULD BE SOLO ?? or google while statements lol
