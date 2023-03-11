28e8cec0a186fb19e1716d6620c9f0541521c794
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-11 14:03:54 -0700

    added * 20 to math.random

--- Develop/script.js
+++ Develop/script.js
@@ -2,15 +2,13 @@
 
 function generatePassword() {
 
-
-
   let characterLower = window.confirm ("Would you like lower case letters?");
   let characterUpper = window.confirm ("Would you like upper case letters?");
   let characterNumbers = window.confirm ("Would you like numbers?");
   let characterSpecial = window.confirm ("Would you like special characters?");
   let characters = window.prompt ("How many characters long would you like your password?");
-  //TODO:Check for input to be correct values 
   var length = characters;
+
   lower = "aghijklwbcmnopqrstuvdefxyz";
   upper = "ASTHIJFGNOZBCDPQUVWRKLMXEY";
   numbers = "1567089234";
@@ -18,82 +16,13 @@ function generatePassword() {
   retVal = "";
 
   for (var i = 0; i < length; i++) {
-    if (characterLower === true) {retVal += lower.charAt(Math.random())}
-    if (characterUpper === true) {retVal += upper.charAt(Math.random())}
-    if (characterNumbers === true) {retVal += numbers.charAt(Math.random())}
-    if (characterSpecial === true) {retVal += special.charAt(Math.random())}
+    if (characterLower === true) {retVal += lower.charAt(Math.random() * 20)}
+    if (characterUpper === true) {retVal += upper.charAt(Math.random() * 20)}
+    if (characterNumbers === true) {retVal += numbers.charAt(Math.random() * 20)}
+    if (characterSpecial === true) {retVal += special.charAt(Math.random() * 20)}
 };
-
-  // const passwordRegularExpressionParamaters = ["characterLower", "characterUpper"]
-
-  // array deoration if the first is clicked/true, then lower case is gonna
-  // equal the string, let uppercase string, number string, special string,
-  // 
-
-  // for (let i = 0; i < passwordRegularExpressionParamaters.length; i++) {
-  //   const element = passwordRegularExpressionParamaters[i];
-  //   generatedpassword += array[i] + lower
-  // }
-
-  // ------------------------------------------------------------------------
-
-  // for (var i = 0, n = characterLength; i < length; ++i) {
-  //     retVal += lower.charAt(Math.random() * n)
-  //     retVal += upper.charAt(Math.random() * n)
-  //     retVal += numbers.charAt(Math.random() * n)
-  //     retVal += special.charAt(Math.random() * n)
-  // };
-
-  // ------------------------------------------------------------------------
-
-  // for (var i = 0, n = characterLength; i < length; ++i) {
-  // do {
-  // retVal += lower.charAt(Math.random() * n)
-  // }
-  // while (characterLower = true);
-
-  // do {
-  // retVal += upper.charAt(Math.random() * n)
-  // }
-  // while (characterUpper = true);
-
-  // do {
-  // retVal += numbers.charAt(Math.random() * n)
-  // }
-  // while (characterNumbers = true);
-
-  // do {
-  // retVal += special.charAt(Math.random() * n)
-  // }
-  // while (characterSpecial = true);
-  // };
-
-
-// ------------------------------------------------------------------------
-
-  // while(characterLower = true) {
-  //   retVal += lower.charAt(Math.random() * n)
-  // }
-
-  // ------------------------------------------------------------------------
-
-  // recieve user input prompt results
-  // while lower result = true, include the lower numbers
-  // while upper result = true, include
-  // while numbers result ttue, include
-  // while special, include
-
-  // ------------------------------------------------------------------------
-
   return retVal.substring(0, length);
 };
-
-// ------------------------------------------------------------------------
-
-// IDEA: SAVE ALL OF THE THINGS AND THEN PUT 'IF TRUE LOWER CASE' ????
-// AND EVERY IF WOULD BE SOLO ?? or google while statements lol
-
-// ------------------------------------------------------------------------
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
